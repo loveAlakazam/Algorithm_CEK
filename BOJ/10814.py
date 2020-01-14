7af2ada0cf7581f2efac449796ce687b28738c36
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2020-01-15 02:01:30 +0900

    Update 10814.py

--- BOJ/10814.py
+++ BOJ/10814.py
@@ -9,10 +9,9 @@ def solution():
         age, name= map(lambda x: int(x) if x.isdigit() else x , input().split())
         d.append([name, age])
     
-    # 딕셔너리 value(나이)를 기준으로 정렬
+    # (나이)를 기준으로 정렬
     d.sort(key=lambda x:x[1])
 
-    # 딕셔너리 d의 key:이름, value:나이
     for name, age in d:
         print(age, name)
     
