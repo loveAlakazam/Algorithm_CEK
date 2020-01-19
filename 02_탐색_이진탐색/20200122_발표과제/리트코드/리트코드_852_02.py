da81e9349a2d743ea9f63e04355482ca560884fb
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2020-01-20 02:50:29 +0900

    Update 리트코드_852_02.py

--- 02_탐색_이진탐색/20200122_발표과제/리트코드/리트코드_852_02.py
+++ 02_탐색_이진탐색/20200122_발표과제/리트코드/리트코드_852_02.py
@@ -3,8 +3,21 @@ class Solution:
         left,right=0, len(A)-1
         while left<right:
             mid=(left+right)//2
-            if A[mid]<A[mid+1]:
+            if A[mid]<A[mid+1]: #오른쪽이 크다면 left를 mid+1인덱스로 한다.
                 left=mid+1
             else:
                 right=mid
-        return left
+        return left #left<right조건이 만족하지 않을 때 left값을 리턴
+    
+    #ex) A=[ 0, 1, 2, 0 ]
+    # step 01
+    # left=0 , right=3 -> mid=1
+    # A[mid]=1, A[mid+1]=2 -> A[mid]<A[mid+1] -> left=mid+1
+    # 
+    # step 02
+    # left=2, right=3 -> mid=2
+    # A[mid]=2, A[mid+1]=0 -> right를 mid로 한다. -> right=mid
+    # 
+    # step 03
+    # left=2, right=2 -> while-loop 조건에 맞지 않음 -> left:2 
+    # A[2] =2 (최댓값)
