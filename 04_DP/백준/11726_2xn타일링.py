811c9d83a714112377eb1d812d8393402a605ebc
loveAlakazam <dmsrkd1216@gmail.com>
2020-01-30 22:05:33 +0900

    DP

---
+++ 04_DP/백준/11726_2xn타일링.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import sys
+def solution(N):
+
+    return dp[N-1]
+    
+def main():
+    N=int(sys.stdin.readline())
+    #(n=1) dp[0]=1
+    dp=[1]*N
+    if N>=2:
+        #(n=2) dp[1]=2
+        dp[1]=2
+
+        #(n=3 ~ n=N) dp[n]=dp[n-1]+dp[n-2]
+        for i in range(2,N):
+            dp[i]=dp[i-1]+dp[i-2]
+    print(solution(N)%10007)
+
+if __name__=='__main__':
+    main()
